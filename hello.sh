e51c6b965b55fcf6b9a70f2f6cd52a3a4c28364a
Franck Villaume <franck.villaume@trivialdev.com>
2021-10-06 10:40:19 +0200

    Update hello.sh
    wait for lock

--- hello.sh
+++ hello.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 if [ -e /etc/debian_version ]; then
+  #get the lock
+  while fuser /var/lib/apt/lists/lock >/dev/null 2>&1 ; do
+    sleep 5;
+  done
   apt-get update && apt-get -y install git default-jre
 else
   yum install git
