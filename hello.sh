cb7b8ba0cc3cd35013609a2b464f91113812f4e7
Franck Villaume <franck.villaume@trivialdev.com>
2021-10-06 08:09:44 +0200

    less is more

--- hello.sh
+++ hello.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 if [ -e /etc/debian_version ]; then
-  if grep -q ^8 /etc/debian_version; then
-    if [ ! -f /etc/apt/sources.list.d/backports.list ]; then
-      echo 'deb http://archive.debian.org/debian jessie-backports main' \
-           > /etc/apt/sources.list.d/backports.list
-    fi
-    if [ ! -f /etc/apt/apt.conf.d/10no--check-valid-until ]; then
-      echo 'Acquire::Check-Valid-Until "0";' \
-           > /etc/apt/apt.conf.d/10no--check-valid-until
-    fi
-    apt-get -y install git
-    apt-get -y install -t jessie-backports openjdk-8-jdk
-  else
-    apt-get -y install git default-jre
-  fi
+  apt-get -y install git default-jre
+else
+  yum install git
 fi
 HOSTNAME=$(hostname)
 hostname -b $HOSTNAME.buildbot.fusionforge.on.azure
