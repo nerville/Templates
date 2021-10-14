6a6e0df1c78a222b9f5fa121ffc0324d19a20e59
Franck Villaume <franck.villaume@trivialdev.com>
2021-10-14 14:26:45 +0200

    Update hello.sh
    try without yum update (stale the waagent)

--- hello.sh
+++ hello.sh
@@ -6,7 +6,7 @@ if [ -e /etc/debian_version ]; then
   done
   apt-get update && apt-get -y upgrade && apt-get -y install default-jre git
 else
-  yum -y update && yum -y install git java-1.8.0-openjdk
+  yum -y install git java-1.8.0-openjdk
 fi
 #force sudoers to handle waagent ...
 echo 'slave ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/zzz-slave
